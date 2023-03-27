45b2da7d95a414499dc2b45c547eeb16022de38d
Aarav Arora <45216129+aaravarora01@users.noreply.github.com>
2023-03-27 08:54:03 -0700

    change stage.java

--- src/main/java/com/nighthawk/spring_portfolio/mvc/checkpoints/Stage.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/checkpoints/Stage.java
@@ -9,8 +9,8 @@ public class Stage {
 
     // Stage data
 	private String name;
-    private Integer stage;
-    private String passwordHash;
+	private int stage;
+	private String passwordHash;
     // name, what number stage you are on, use list of levels, make main methods
     // to string to json
 }
